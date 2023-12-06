f59d288cf3b7705ba2db2165f7aa8b15f3f006f0
Matt King <matt.king@inmoment.com>
2023-12-06 09:07:35 -0500

    tutorial

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>This is the about page for the Redwood blog tutorial.</p>
+    </>
+  )
+}
+
+export default AboutPage
